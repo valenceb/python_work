91dc1391c01ad6b360c0fdc376b1b25ba5824c1e
valenceb <valence.bai@qq.com>
2018-08-18 22:47:12 +0800

    1st edit

--- crossSprintPlan.py
+++ crossSprintPlan.py
@@ -1,23 +1,69 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = "valenceb"
+
 from openpyxl import Workbook
 import datetime
 import csv
+import urllib.request
+import re
 
-wb = Workbook()
 
-# grab the active worksheet
-ws = wb.active
+def string2Date(dateStr):
+    dateStr = str(datetime.datetime.now().year) + '/' + dateStr
+    date = datetime.datetime.strptime(dateStr, '%Y/%m/%d')
+    return date
 
-# Data can be assigned directly to cells
-ws['A1'] = 42
 
-# Rows can also be appended
-ws.append([1, 2, 3])
+def getHtml(url):
+    try:
+        page = urllib.request.urlopen(url)
+        html = page.read()
+        html = str(html)
+        return html
+    except Exception as e:
+        print(e.reason)
+        return False
 
-# Python types will automatically be converted
-ws['A2'] = datetime.datetime.now()
 
-# Save the file
-wb.save("output/sample.xlsx")
+def inSprint(sprint):
+    return
+
+
+class Project:
+    def __init__(self, wiki):
+        self.wiki = wiki
+        html = getHtml(wiki)
+        reg = 'Project Title Regular Formular'
+        self.title = re.search(reg, html).group()
+        self.row = 0
+
+
+class Sprint:
+    def __init__(self, sprintName):
+        reg = r'Sprint (\d+)\((\d+/\d+)-(\d+/\d+)\)'
+        searchObj = re.search(reg, sprintName, re.M | re.I)
+        startDate = searchObj.group(2)
+        endDate = searchObj.group(3)
+        self.sprintNum = searchObj.group(1)
+        self.startDate = string2Date(startDate)
+        self.endDate = string2Date(endDate)
+        self.column = 0
+        now = datetime.datetime.now()
+        if self.startDate<now and self.endDate>now:
+            self.isCurSprint = True
+        else:
+            self.isCurSprint = False
+
+
+class Backlog:
+    def __init__(self, checkItem):
+        reg = 'Identify date/description'
+        self.date = ""
+        self.description = ""
+        self.column = 0
+        self.row = 0
+
 
 if __name__ == '__main__':
     projectList = []
@@ -31,4 +77,6 @@ if __name__ == '__main__':
         empf = csv.reader(f)
         for emp in empf:
             sprintList.append(emp)
-    print(sprintList)
+    sprint1 = Sprint(''.join(sprintList[0]))
+    print(sprint1.startDate)
+    print(sprint1.endDate)
