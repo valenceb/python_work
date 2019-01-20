c6ef2ffac9c79d1e19e5755656090dbc19eb39bc
valenceb <valence.bai@qq.com>
2019-01-20 08:41:11 +0800

    a

--- luyilu.py
+++ luyilu.py
@@ -5,7 +5,25 @@ import time
 from io import BytesIO
 from PIL import Image
 
-number = 1
+
+class PicSite:
+    def __init__(self, url):
+        self.url = url
+        self.subUrl = ''
+        self.picCount = 1
+        self.nvyouID = 1
+        path = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        path = "output/luyilu_" + path
+        self.path = path
+
+    def getDomain(self):
+        reg = '(https://.+?)/.+'
+        dmn = re.findall(reg, self.url)
+        if len(dmn) > 0:
+            dmnstr = dmn[0]
+            return dmnstr
+        else:
+            return self.url
 
 
 def getHtml(url):
@@ -31,14 +49,14 @@ def mkdir(path):
         return False
 
 
-def saveImages(imglist, name, nvyouID):
-    global number
+def saveImages(imglist, picSite):
     for imageURL in imglist:
         splitPath = imageURL.split('.')
         fTail = splitPath.pop()
+        imageURL = imageURL if imageURL[0:4] == "http" else picSite.getDomain() + imageURL
         if len(fTail) > 3:
             fTail = 'jpg'
-        fileName = name + "/" + str(nvyouID) + '_' + ("%03d" % number) + "." + fTail
+        fileName = picSite.path + "/" + str(picSite.nvyouID) + '_' + ("%03d" % picSite.picCount) + "." + fTail
         try:
             rq = urllib.request.Request(imageURL)
             u = urllib.request.urlopen(rq)
@@ -56,7 +74,7 @@ def saveImages(imglist, name, nvyouID):
         except urllib.error.HTTPError as e:
             print(imageURL)
             print(e.reason)
-        number += 1
+        picSite.picCount += 1
 
 
 # get all the image url from html
@@ -66,29 +84,29 @@ def getAllImg(html):
     return imglist
 
 
-def crawling(url, path, nvyouID):
-    print("Crawling " + url)
-    srcHtml = getHtml(url)
+def crawling(picSite):
+    print("Crawling " + picSite.subUrl)
+    srcHtml = getHtml(picSite.subUrl)
     if not srcHtml:
         return False
     imglist = getAllImg(srcHtml)
-    saveImages(imglist, path, nvyouID)
+    saveImages(imglist, picSite)
     return True
 
 
-def crawling_by_category(url, index):
-    global number
-    number = 1
-    crawling(url + str(index) + '.html', path, index)
+def crawling_by_category(picSite):
+    picSite.picCount = 1
+    picSite.subUrl = picSite.url + str(picSite.nvyouID) + '.html'
+    crawling(picSite)
     for value in range(2, 30):
-        if not crawling(url + str(index) + '_' + str(value) + '.html', path, index): break
+        picSite.subUrl = picSite.url + str(picSite.nvyouID) + '_' + str(value) + '.html'
+        if not crawling(picSite): break
 
 
 if __name__ == '__main__':
-    url = "https://96xx2019.com/luyilu/"
-    path = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-    path = "output/luyilu_" + path
-    mkdir(path)
-    for value in range(1262, 1900):
-        crawling_by_category(url, value)
+    picSite = PicSite("https://96xx2019.com/luyilu/")
+    mkdir(picSite.path)
+    for value in range(1266, 1900):
+        picSite.nvyouID = value
+        crawling_by_category(picSite)
     print("Process Completed Successfully.")
