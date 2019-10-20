168d30bf54c0f2fe5590199dbf6c9ed7c695e4f9
valenceb <valence.bai@qq.com>
2019-10-20 16:12:23 +0800

    a

--- picBrowser.py
+++ picBrowser.py
@@ -1,41 +1,115 @@
 import tkinter as tk, os
+from io import BytesIO
+
 from PIL import Image, ImageTk
+import re
+import urllib.request
+
+
+def getHtml(url):
+    try:
+        page = urllib.request.urlopen(url)
+        html = page.read()
+        html = str(html)
+        return html
+    except urllib.error.HTTPError as e:
+        print(e.reason)
+        return False
+
+
+def getAllImg(html):
+    reg = r'src=\"(.{0,100}\.jpg)\"'
+    imglist = re.findall(reg, html)
+    return imglist
+
+
+def getNvyouIDs(html):
+    reg = r'/luyilu/(.{0,100}).html'
+    nvyouIDList = re.findall(reg, html)
+    nvyouIDList = list(map(int, nvyouIDList))
+    nvyouIDList = list(set(nvyouIDList))
+    nvyouIDList.sort()
+    nvyouIDList.reverse()
+    return nvyouIDList
+
+
+def getImage(url):
+    rq = urllib.request.Request(url)
+    u = urllib.request.urlopen(rq)
+    data = u.read()
+    img = Image.open(BytesIO(data))
+    if img.size[0] <= 430:
+        return False
+    return ImageTk.PhotoImage(img)
+
+
+class PicSite:
+    def __init__(self, url):
+        self.url = url
+        self.subUrl = ''
+        self.nvyouIDs = getNvyouIDs(getHtml(url))
+        self.image = ''
+        self.nPerPage = 0
+
+    def crawling_by_category(self):
+        for nvyouID in self.nvyouIDs:
+            self.subUrl = self.url + str(nvyouID) + '.html'
+            # crawling(picSite)
+            for value in range(1, 30):
+                if value != 1:
+                    self.subUrl = self.url + str(nvyouID) + '_' + str(value) + '.html'
+                print("Crawling " + self.subUrl)
+                srcHtml = getHtml(self.subUrl)
+                if not srcHtml:
+                    return False
+                imglist = getAllImg(srcHtml)
+                if len(imglist) > 0 and imglist[
+                    0] == 'https://www.images.96xxpic.com:8819/allimg/161029/1-1610292146350-L.jpg':
+                    return False
+                #每页只显示三张
+                self.nPerPage = 0
+                for il in imglist:
+                    self.nPerPage += 1
+                    if self.nPerPage >= 4: break
+                    photoImage = getImage(il)
+                    if not photoImage: continue
+                    yield photoImage
+                    print("peek " + il)
+
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        self.files = os.listdir(r'D:\Picture')
-        self.index = 0
-        img = Image.open(r'D:\Picture' + '\\' + self.files[self.index])  # 打开图片
-        self.img = ImageTk.PhotoImage(img)  # 用PIL模块的PhotoImage打开
         tk.Frame.__init__(self, master)
         self.pack()
+        self.picSite = PicSite("https://96xx2019.com/luyilu/")
+        self.crawlingGenerator = self.picSite.crawling_by_category()
+        self.img = next(self.crawlingGenerator)
         self.createWidgets()
 
     def createWidgets(self):
-        # self.f = tk.Frame()
-        # self.f.pack()
-        self.btnPrev = tk.Button(self, text='Prev', command=self.prev)
-        self.btnPrev.pack(side=tk.LEFT)
+        # self.btnPrev = tk.Button(self, text='Prev', command=self.prev)
+        # self.btnPrev.pack(side=tk.TOP)
         self.btnNext = tk.Button(self, text='Next', command=self.next)
-        self.btnNext.pack(side=tk.RIGHT)
+        self.btnNext.pack(side=tk.TOP)
+        self.btnNext = tk.Button(self, text='Next Page', command=self.nextPage)
+        self.btnNext.pack(side=tk.TOP)
         self.lblImage = tk.Label(self)
         self.lblImage['image'] = self.img
         self.lblImage.pack()
 
-    def prev(self):
-        self.showfile(-1)
+    # def prev(self):
+    #     self.showfile(-1)
 
     def next(self):
-        self.showfile(1)
+        self.showfile()
 
-    def showfile(self, n):
-        self.index += n
-        if self.index < 0:
-            self.index = len(self.files) - 1
-        if self.index > (len(self.files) - 1):
-            self.index = 0
-        img = Image.open(r'D:\Picture' + '\\' + self.files[self.index])
-        self.img = ImageTk.PhotoImage(img)
+    def nextPage(self):
+        self.picSite.nPerPage = 4 #直接翻页
+        self.showfile()
+
+    def showfile(self):
+        self.img = next(self.crawlingGenerator)
         self.lblImage['image'] = self.img
 
 
