f046ce14bc96733485d52f11a05783bf51b26000
valenceb <35684640+valenceb@users.noreply.github.com>
2019-10-21 11:55:19 +0800

    Update webBrowser.py

--- webBrowser.py
+++ webBrowser.py
@@ -1,2 +1,5 @@
-import os
-print(os.getcwd())
+from skimage import io
+img_src = 'http://wx2.sinaimg.cn/mw690/ac38503ely1fesz8m0ov6j20qo140dix.jpg'
+image = io.imread(img_src)
+io.imshow(image)
+io.show()
