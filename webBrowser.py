a40477c9d8fc674b51099107a8b6648ced3be72d
valenceb <35684640+valenceb@users.noreply.github.com>
2019-10-21 10:29:18 +0800

    Update webBrowser.py

--- webBrowser.py
+++ webBrowser.py
@@ -1,10 +1,2 @@
-
-import sys
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWebEngineWidgets import QWebEnginePage, QWebEngineView
-app = QApplication(sys.argv)
-browser = QWebEngineView()
-browser.load(QUrl("http://www.baidu.com/"))
-browser.show()
-app.exec_()
+import webbrowser
+webbrowser.open('http://www.baidu.com/')
