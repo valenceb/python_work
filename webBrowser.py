29cd50ab101c88e96da92729bb14ad28edc0a5c3
valenceb <35684640+valenceb@users.noreply.github.com>
2019-10-22 14:48:07 +0800

    Update webBrowser.py

--- webBrowser.py
+++ webBrowser.py
@@ -1,5 +1,3 @@
-from skimage import io
-img_src = 'http://wx2.sinaimg.cn/mw690/ac38503ely1fesz8m0ov6j20qo140dix.jpg'
-image = io.imread(img_src)
-io.imshow(image)
-io.show()
+import webbrowser as web
+#new:0/1/2 0：同一浏览器窗口打开 1：打开浏览器新的窗口，2：打开浏览器窗口新的tab
+web.open('http://wx2.sinaimg.cn/mw690/ac38503ely1fesz8m0ov6j20qo140dix.jpg',new=0,autoraise=True)
