27776907c826c51b09809ddaedd8acf8f98737d3
valenceb <valence.bai@ehealth.com>
2019-11-11 09:51:29 +0800

    a

--- appsec_matrix/issueTurnaround.py
+++ appsec_matrix/issueTurnaround.py
@@ -1,7 +1,7 @@
 from jiraOps import JiraOperator, JiraIssue
 import pyecharts.options as opts
 import csv
-from pyecharts.charts import Line
+from pyecharts.charts import Line, Scatter
 from pyecharts.globals import ThemeType
 
 
@@ -9,6 +9,7 @@ def areaChart(prodLine, issueList) -> Line:
     x = []
     y1_TurnaroundTime = []
     y2_IdleTime = []
+    y3_ClosedTime = []
     for issue in issueList:
         # 坐标刻度竖排
         # keyStr = ""
@@ -18,20 +19,27 @@ def areaChart(prodLine, issueList) -> Line:
         x.append(issue.key)
         y1_TurnaroundTime.append(issue.turnaroundTime)
         y2_IdleTime.append(issue.idleTime)
+        y3_ClosedTime.append(issue.closeTime)
     c = (
         # Line(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
         Line()
             .add_xaxis(x)
             .add_yaxis(
-            "Turnaround Time", y1_TurnaroundTime, areastyle_opts=opts.AreaStyleOpts(opacity=0.5)
+            "In Progress", y1_TurnaroundTime, areastyle_opts=opts.AreaStyleOpts(opacity=0.5)
         )
             .add_yaxis(
-            "Idle Time", y2_IdleTime, areastyle_opts=opts.AreaStyleOpts(opacity=0.5)
+            "Idle Time", y2_IdleTime, areastyle_opts=opts.AreaStyleOpts(opacity=0.4)
         )
             .set_global_opts(title_opts=opts.TitleOpts(title=prodLine + " Security Turnaround (days)",
                                                        subtitle="Including issues' idle time and overall turnaround time."),
-                             legend_opts=opts.LegendOpts(pos_left="60%", pos_top="8%"))
+                             legend_opts=opts.LegendOpts(pos_left="60%", pos_top="6%"))
     )
+    scatter = (
+        Scatter()
+            .add_xaxis(x)
+            .add_yaxis("Closed", y3_ClosedTime, markpoint_opts=opts.MarkPointOpts(symbol='diamond'))
+    )
+    c.overlap(scatter)
     c.render("output/" + prodLine + "_area_chart.html")
 
     with open('output/' + prodLine + '_turnaround.csv', 'w', newline='')as f:
@@ -39,7 +47,8 @@ def areaChart(prodLine, issueList) -> Line:
         data = [
             x,
             y2_IdleTime,
-            y1_TurnaroundTime
+            y1_TurnaroundTime,
+            y3_ClosedTime
         ]
         csvfile.writerows(data)
         f.close()
@@ -56,5 +65,21 @@ def prodLineAreaChart(prodLine, jql):
 
 
 if __name__ == '__main__':
+
+    jql = "issue in linkedIssues('ASEC-201') AND Project = 'Back Office CORE team' AND 'Target Release Date' <= endOfMonth('-1M') and 'Target Release Date' >= endofMonth('-5M') ORDER BY Severity, 'CVSS Score', Created DESC"
+    prodLineAreaChart("BackOffice", jql)
+
+    jql = "issue in linkedIssues('ASEC-201') AND Project = 'Carrier Integration' AND 'Target Release Date' <= endOfMonth('-1M') and 'Target Release Date' >= endofMonth('-5M') ORDER BY Severity, 'CVSS Score', Created DESC"
+    prodLineAreaChart("Carrier Integration", jql)
+
+    jql = "issue in linkedIssues('ASEC-201') AND Project = 'Yoda - IFP Front Office' AND 'Target Release Date' <= endOfMonth('-1M') and 'Target Release Date' >= endofMonth('-5M') ORDER BY Severity, 'CVSS Score', Created DESC"
+    prodLineAreaChart("IFP", jql)
+
     jql = "issue in linkedIssues('ASEC-201') AND Project = 'Medicare Scrum Project' AND 'Target Release Date' <= endOfMonth('-1M') and 'Target Release Date' >= endofMonth('-5M') ORDER BY Severity, 'CVSS Score', Created DESC"
     prodLineAreaChart("Medicare", jql)
+
+    jql = "issue in linkedIssues('ASEC-201') AND Project in ('Platform Service', 'Product Center', 'Financial Systems and Informational Systems') AND 'Target Release Date' <= endOfMonth('-1M') and 'Target Release Date' >= endofMonth('-5M') ORDER BY Severity, 'CVSS Score', Created DESC"
+    prodLineAreaChart("Platform", jql)
+
+    jql = "issue in linkedIssues('ASEC-201') AND Project = 'VADR - SMB Front Office' AND 'Target Release Date' <= endOfMonth('-1M') and 'Target Release Date' >= endofMonth('-5M') ORDER BY Severity, 'CVSS Score', Created DESC"
+    prodLineAreaChart("SMB", jql)
--- appsec_matrix/jiraOps.py
+++ appsec_matrix/jiraOps.py
@@ -10,6 +10,8 @@ class JiraIssue:
         self.key = key
         self.turnaroundTime = turnaroundTime
         self.idleTime = idleTime
+        self.closeTime = ""
+        self.summary = ""
 
 
 class JiraOperator:
@@ -25,7 +27,7 @@ class JiraOperator:
         self.issueDone = ""
         f_obj.close()
 
-    def turnaroundTime(self, issueNo):
+    def turnaroundTime(self, jiraIssue):
         self.issueCreated = ""  # reset the values
         self.issueDone = ""  # reset the values
         headers = {
@@ -33,21 +35,24 @@ class JiraOperator:
             'content-type': "application/json"
         }
         requests.packages.urllib3.disable_warnings()
-        url = self.jiraUrl + "/rest/api/2/issue/{}?expand=changelog".format(issueNo)
+        url = self.jiraUrl + "/rest/api/2/issue/{}?expand=changelog".format(jiraIssue.key)
         response = requests.get(url, headers=headers, verify=False,
                                 auth=HTTPBasicAuth(self.jiraUser, self.jiraPwd))
+        issueClosed = False
         if response.status_code == 200:
             self.issueCreated = response.json()["fields"]["created"]
-            print(issueNo + " was created at " + self.issueCreated)
+            print(jiraIssue.key + " was created at " + self.issueCreated)
             histories = response.json()["changelog"]["histories"]
             for history in histories:
                 if "8.0 Current Release" == history["items"][-1]["toString"]:
                     self.issueDone = history["created"]
-                    print(issueNo + " was released at " + history["created"])
+                    issueClosed = True
+                    print(jiraIssue.key + " was released at " + history["created"])
                     break
                 if "Closed" == history["items"][-1]["toString"]:
                     self.issueDone = history["created"]
-                    print(issueNo + " was closed at " + history["created"])
+                    issueClosed = True
+                    print(jiraIssue.key + " was closed at " + history["created"])
                     break
             self.issueDone = str(datetime.datetime.now()) if self.issueDone == "" else self.issueDone
             if self.issueCreated != "":
@@ -55,14 +60,14 @@ class JiraOperator:
                 d2 = datetime.datetime.strptime(self.issueDone[:10], '%Y-%m-%d')
                 delta = d2 - d1
                 print("Turnaround time for " + str(delta.days) + " days.")
-                return delta.days
-        return 0
+                jiraIssue.turnaroundTime = delta.days
+        jiraIssue.closeTime = jiraIssue.turnaroundTime if issueClosed else jiraIssue.closeTime
 
     def turnaroundTimeBatch(self, issueList):
         for jiraIssue in issueList:
-            jiraIssue.turnaroundTime = self.turnaroundTime(jiraIssue.key)
+            self.turnaroundTime(jiraIssue)
 
-    def idleTime(self, issueNo):
+    def idleTime(self, jiraIssue):
         self.issueCreated = ""  # reset the values
         self.issueStarted = ""  # reset the values
         headers = {
@@ -70,21 +75,21 @@ class JiraOperator:
             'content-type': "application/json"
         }
         requests.packages.urllib3.disable_warnings()
-        url = self.jiraUrl + "/rest/api/2/issue/{}?expand=changelog".format(issueNo)
+        url = self.jiraUrl + "/rest/api/2/issue/{}?expand=changelog".format(jiraIssue.key)
         response = requests.get(url, headers=headers, verify=False,
                                 auth=HTTPBasicAuth(self.jiraUser, self.jiraPwd))
         if response.status_code == 200:
             self.issueCreated = response.json()["fields"]["created"]
-            print(issueNo + " was created at " + self.issueCreated)
+            print(jiraIssue.key + " was created at " + self.issueCreated)
             histories = response.json()["changelog"]["histories"]
             for history in histories:
                 if ("2.0 In Progress" == history["items"][-1]["toString"]):
                     self.issueStarted = history["created"]
-                    print(issueNo + " was in progress at " + history["created"])
+                    print(jiraIssue.key + " was in progress at " + history["created"])
                     break
                 if "Closed" == history["items"][-1]["toString"]:
                     self.issueStarted = history["created"]
-                    print(issueNo + " was closed at " + history["created"])
+                    print(jiraIssue.key + " was closed at " + history["created"])
                     break
             self.issueStarted = str(datetime.datetime.now()) if self.issueStarted == "" else self.issueStarted
             if self.issueCreated != "" and self.issueStarted != "":
@@ -97,7 +102,25 @@ class JiraOperator:
 
     def idleTimeBatch(self, issueList):
         for jiraIssue in issueList:
-            jiraIssue.idleTime = self.idleTime(jiraIssue.key)
+            jiraIssue.idleTime = self.idleTime(jiraIssue)
+
+    def summary(self, jiraIssue):
+        headers = {
+            "X-Atlassian-Token": "no-check",
+            'content-type': "application/json"
+        }
+        requests.packages.urllib3.disable_warnings()
+        url = self.jiraUrl + "/rest/api/2/issue/{}".format(jiraIssue.key)
+        response = requests.get(url, headers=headers, verify=False,
+                                auth=HTTPBasicAuth(self.jiraUser, self.jiraPwd))
+        if response.status_code == 200:
+            return response.json()["fields"]["summary"]
+        else:
+            return ""
+
+    def summaryBatch(self, issueList):
+        for jiraIssue in issueList:
+            jiraIssue.summary = self.summary(jiraIssue)
 
     def jqlSearch(self, jqlString):
         headers = {
@@ -122,7 +145,7 @@ class JiraOperator:
         issues = response.json()["issues"]
         jiraIssues = []
         for issue in issues:
-            ji = JiraIssue(issue["key"], self.turnaroundTime(issue["key"]))
+            ji = JiraIssue(issue["key"])
             jiraIssues.append(ji)
             # print (ji.key + str(ji.turnaroundTime))
         return jiraIssues
@@ -130,5 +153,5 @@ class JiraOperator:
 
 if __name__ == '__main__':
     jiraOperator = JiraOperator()
-    jiraOperator.turnaroundTime("YODA-10555")
+    # jiraOperator.turnaroundTime("YODA-10555")
     ji = jiraOperator.jqlSearch("project=YODA")
